ac43cbd0c7a8dc3d11bd9b7e5fa50aaa47cc9491
Ryan Prince <ryanprince@Ryans-MacBook-Air-2.local>
2022-10-19 17:11:31 -0400

    tested concat and append functions

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-df1_original = pd.read_csv('./Projects/Hills Pet - 2022 PD Handraiser.csv')
-df3_original = pd.read_csv('./Projects/1381146_Hills_Pet_-_2022_PD_Handraiser_20221001_031153_3858949580.csv')
+df1_original = pd.read_csv('/Users/ryanprince/Desktop/Projects/Combine-Reports/Hills Pet - 2022 PD Handraiser.csv')
+df3_original = pd.read_csv('/Users/ryanprince/Desktop/Projects/Combine-Reports/1381146_Hills_Pet_-_2022_PD_Handraiser_20221001_031153_3858949580.csv')
 
 # filter data by two conditions
 # print(df1.loc[(df1['Placement'] == 'P1W6PT2_HLN_PD_018_Content - Custom_CLUEP_Derm Cat 300x250_Run of Network_Demo_P25+_300 x 250_Standard_Other_NA') & (df1['Total Cost'] > )])
@@ -23,8 +23,11 @@ df1['CTR'] = df1['CTR'].str.replace('%', '')
 # change values in 'CTR' from string (object) to float
 df1['CTR'] = df1['CTR'].astype(float)
 
-# change values in 'CTR' to decimal form by dividing by 100
+# change values in 'CTR' to true decimal form by dividing by 100
 df1['CTR'] = df1['CTR']/100
+
+# get list of unique dates
+# unique_dates = df1.Date.unique()
 
 # get sum of impressions, sum of clicks, avg CTR, and sum cost by placement
 df1 = df1.groupby(['Placement'], as_index=False).agg(SumImpressions=('Impressions', 'sum'),
@@ -34,7 +37,7 @@ df1 = df1.groupby(['Placement'], as_index=False).agg(SumImpressions=('Impression
 
 
 # update third party (DCM) report
-# change values in 'Click Rate to accurate decimal amount by dividing by 100
+# change values in 'Click Rate' to true decimal amount by dividing by 100
 df3['Click Rate'] = df3['Click Rate']/100
 
 # add new calculated column 'Spend' 
@@ -50,8 +53,16 @@ df3 = df3.groupby(['Placement'], as_index=False).agg(DCM_SumImpressions=('Impres
 # merge dataframes 
 df_merged = pd.merge(df1, df3)
 
-df_placements = pd.DataFrame(columns=['Placement', '1P_Impressions', '1P_Clicks', '1P_CTR', '1P_Spend', '3P_Impressions', '3P_Clicks', '3P_CTR', '3P_Spend'] )
-placements = ['Weight Dog', 'Weight Cat', 'Derm Dog', 'Derm Cat', 'Renal Dog', 'Renal Cat','GI Dog GIB', 'GI Cat GIB', 'Dental Dog', 'Dental Cat', 'Urinary Dog', 'Urinary Cat', 'GI Dog ID', 'GI Cat ID']
+# output data to csv file
+df_merged.to_csv('new_test.csv')
+
+
+# df_concat = pd.concat([df1, df3], ignore_index=False)
+# df_concat.to_csv('concat_test.csv')
+
+
+# df_placements = pd.DataFrame(columns=['Placement', '1P_Impressions', '1P_Clicks', '1P_CTR', '1P_Spend', '3P_Impressions', '3P_Clicks', '3P_CTR', '3P_Spend'] )
+# placements = ['Weight Dog', 'Weight Cat', 'Derm Dog', 'Derm Cat', 'Renal Dog', 'Renal Cat','GI Dog GIB', 'GI Cat GIB', 'Dental Dog', 'Dental Cat', 'Urinary Dog', 'Urinary Cat', 'GI Dog ID', 'GI Cat ID']
 
 
 
@@ -65,13 +76,13 @@ placements = ['Weight Dog', 'Weight Cat', 'Derm Dog', 'Derm Cat', 'Renal Dog', '
 #             # impressions += df_merged[i.SumImpressions]
 #             print(df_merged[i.SumImpressions])
 
-for placement in placements:
-    for index, row in df_merged.iterrows():
-        impressions = 0
-        print(impressions)
-        if placement in row['Placement']:
-            # print(row['SumImpressions'])
-            impressions += row['SumImpressions']
+# for placement in placements:
+#     for index, row in df_merged.iterrows():
+#         impressions = 0
+#         print(impressions)
+#         if placement in row['Placement']:
+#             # print(row['SumImpressions'])
+#             impressions += row['SumImpressions']
 
 # sum_impressions = 0
 # for i in df_merged.itertuples():
@@ -95,7 +106,7 @@ for placement in placements:
 # print(df_placements)
 
 # print(df1)
-# df_merged.to_csv('new_test.csv')
+
 
 # .sort_values('Placement')
 
